0176f94000801275e37dc36e84206449cfef93be
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-24 13:21:04 -0500

    push working version without routes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', ['$scope', '$http',function MyController($scope, $http){
-  var apiKey = '';
+  var apiKey = 'c4edec026df80c72268790750153e92b%3A8%3A48762272';
   var url = 'http://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=' + apiKey;
   $http.get(url).success(function(data){
      $scope.reviews = data.results;
