37fc5b2a6f6b38ae1b8c31be9bd87b3e3563e4e1
Benjamin Aplin <benjaminaplin@gmail.com>
2015-12-01 23:48:20 -0500

    get api working

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,48 @@
-var myApp = angular.module('myApp', [
-  'ngRoute',
-  'reviewControllers'
-]);
 
-myApp.config(['$routeProvider', function($routeProvider) {
+angular.module('reviewApp', ['ngRoute'])
+
+.controller('ControllerReviewList', ['ReviewService', function (ReviewService){
+  var vm = this;
+  vm.artistOrder = 'name';
+  vm.list = function(){
+    return ReviewService.listReviews();
+  }
+  var init = function(){
+    return ReviewService.getReviews();
+  };
+  init();
+}])
+
+.controller('ControllerReviewDetail', [ '$routeParams', function($scope, $routeParams){
+  var vm = this;
+  this.reviewId = $routeParams.reviewId;
+}])
+
+.service('ReviewService', ['$http', function($http){
+  var reviews = []
+  this.getReviews = function(){
+    $http.get('/api/reviews').then(function(res){
+      reviews = res.data;
+      console.log('got reviews from backend upon load');
+    }, function(errResponse){
+       console.error('reviews query error')
+    });
+  }
+  this.listReviews = function(){
+    console.log('your listin reviews!')
+    return reviews;
+  };  
+}])
+
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   when('/reviews', {
     templateUrl: 'js/partials/review-list.html',
-    controller: 'ReviewListController'
+    controller: 'ControllerReviewList'
   }).
   when('/reviews/:reviewId', {
     templateUrl: 'js/partials/review-detail.html',
-    controller: 'ReviewDetailController'
+    controller: 'ControllerReviewDetail'
   }).
   otherwise({
     redirectTo: '/reviews'
